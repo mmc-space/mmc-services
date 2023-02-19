ea2c6e9db692a914d30b408f79830d3788d43f6c
codeDaiQin <1046572917@qq.com>
2023-02-20 03:09:20 +0800

    feat: 🔥 server entry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
 
 import { AppController } from './app.controller'
 import { ChatModule } from './modules/chat/chat.module'
 
 @Module({
   imports: [
+    // https://docs.nestjs.com/security/rate-limiting
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ConfigModule.forRoot({
       envFilePath: ['.development.env', '.production.env'],
       isGlobal: true,
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger, ValidationPipe } from '@nestjs/common'
+import helmet from 'helmet'
+
 import * as pkg from '../package.json'
 import { AppModule } from './app.module'
 import { ResponseInterceptor } from '@/common/interceptors/response.interceptors'
 import { AllExceptionsFilter } from '@/common/exceptions/base.exception.filter'
-import { config, isDev } from '@/config'
+import { config } from '@/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  if (!isDev) app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api')
 
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new ResponseInterceptor())
   app.useGlobalFilters(new AllExceptionsFilter())
+
+  app.use(helmet())
 
   await app.listen(config.port, '0.0.0.0')
 
