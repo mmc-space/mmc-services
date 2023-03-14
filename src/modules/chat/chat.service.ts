8c72d8926da71729c933947c75ba892597d48700
戴勤 <1046572917@qq.com>
2023-03-14 14:21:27 +0800

    feat: chatgpt api

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import Authenticator from 'openai-token'
+// import Authenticator from 'openai-token'
 import { importDynamic } from '@/utils/import'
 
 @Injectable()
@@ -9,14 +9,15 @@ export class ChatService {
   constructor(private configService: ConfigService) {}
 
   async initChatgpt() {
-    const email = this.configService.get<string>('CHATGPT_EMAIL')
-    const password = this.configService.get<string>('CHATGPT_PASSWORD')
-    const auth = new Authenticator(email, password)
-    await auth.begin()
-    const accessToken = await auth.getAccessToken()
-    const { ChatGPTUnofficialProxyAPI } = await importDynamic('chatgpt')
+    const apiKey = this.configService.get<string>('CHATGPT_KEY')
+    // const email = this.configService.get<string>('CHATGPT_EMAIL')
+    // const password = this.configService.get<string>('CHATGPT_PASSWORD')
+    // const auth = new Authenticator(email, password)
+    // await auth.begin()
+    // const accessToken = await auth.getAccessToken()
+    const { ChatGPTAPI } = await importDynamic('chatgpt')
 
-    const chatgpt = new ChatGPTUnofficialProxyAPI({ accessToken })
+    const chatgpt = new ChatGPTAPI({ apiKey })
 
     this.chatgpt = chatgpt
   }
